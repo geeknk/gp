3d1c42be7aedd0e1507723fbd0a1e5f7532887b1
aayushgetflair <aayush@getflair.io>
2024-12-18 13:34:25 +0530

    added initial code

---
+++ api/admin-api/controllers/auth.controller.js
@@ -0,0 +1,42 @@
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+export const signup = async (req, res, next) => {
+  const { username, email, password, role } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword, role });
+  try {
+    await newUser.save();
+    res.status(201).json("New user created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return res.status(404).json({message:'User not found'})
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return res.status(400).json({message:'Invalid Credentials'})
+    const token = jwt.sign(
+      { id: validUser._id, role: validUser.role },
+      process.env.JWT_SECRET_KEY
+    );
+    const { password: pass, ...rest } = validUser._doc;
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signout = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token")
+    res.status(200).json({ message: "User has been signed out..." })
+  } catch (error) {
+    next(error)
+  }
+}
